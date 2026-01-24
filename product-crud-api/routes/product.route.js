0de82beef51a4f392c1c61877fa966bad53e165e
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-01-24 17:35:15 +0100

    add images to products

--- product-crud-api/routes/product.route.js
+++ product-crud-api/routes/product.route.js
@@ -7,10 +7,14 @@ const {
   deleteProductByID,
 } = require('../controllers/product.controller');
 const checkAuth = require('../middlewares/check-auth');
+const { upload } = require('../lib/multer');
 
 const router = express.Router();
 
-router.route('/').post([checkAuth, addProduct]).get(getProducts);
+router
+  .route('/')
+  .post([checkAuth, upload.single('image'), addProduct])
+  .get(getProducts);
 
 router
   .route('/:productId')
